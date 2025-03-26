c7bb60c6585aec11bb5c9ec6a635acbf6aa7291f
Parthikan <parthikankanagaraj@gmail.com>
2025-03-26 22:45:45 +0530

    Updated Student.php

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -1,8 +1,13 @@
 <?php
 
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
+    use HasFactory;
+
     protected $fillable = ['name', 'department', 'gender', 'skill'];
 }
